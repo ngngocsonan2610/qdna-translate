db84a761205cfbf1208045fac9337d32c65b4722
Nguyen Ngoc Son An <ngngocsonan2610@gmail.com>
2019-06-22 11:49:13 +0700

    Update trans.py

--- trans.py
+++ trans.py
@@ -18,7 +18,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///app.db'
 db = SQLAlchemy(app)
 
 db.engine.execute(" DELETE FROM chat ")
