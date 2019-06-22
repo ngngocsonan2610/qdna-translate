e3f5cffcb5683d1479aa0f9bc7423459a5a5a9f8
Nguyen Ngoc Son An <ngngocsonan2610@gmail.com>
2019-06-22 12:54:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,12 +12,13 @@ def translate(eng_text):
   return translations.text
 
 from flask import Flask , render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-from flask_heroku import Heroku
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
-app = Flask( __name__ )
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-heroku = Heroku(app)
 db = SQLAlchemy(app)
 
 db.engine.execute(" DELETE FROM chat ")
