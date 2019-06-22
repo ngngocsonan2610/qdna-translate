b93f24bbf6de400a552fc457eaa5def678003138
Nguyen Ngoc Son An <ngngocsonan2610@gmail.com>
2019-06-22 12:34:42 +0700

    Update trans.py

--- trans.py
+++ trans.py
@@ -8,26 +8,31 @@ def translate(eng_text):
   translator = Translator(service_urls=[
       'translate.google.com',
       'translate.google.co.vi',    ])
-  translations = translator.translate(eng_text, dest= language_code)
-  
+  translations = translator.translate(eng_text, dest= language_code)  
   return translations.text
 
 from flask import Flask , render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+import sys
+import json
+from flask_heroku import Heroku
 
-
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///app.db'
+app = Flask( __name__ )
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+heroku = Heroku(app)
 db = SQLAlchemy(app)
 
 db.engine.execute(" DELETE FROM chat ")
 
 
 class Chat(db.Model):
+    __tablename__ = "dataentry"
     id = db.Column(db.Integer, primary_key = True)
     text = db.Column(db.String(200))
     mess_class = db.Column(db.String(10))
+    
+    def __init__(self, mydata):
+      self.mydata = mydata
     
 @app.route('/')
 def index():
